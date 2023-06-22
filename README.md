7b8e58ad98ef7cfde1411bd43f063ef8d6c76af3
rbamarc <rbamarc929@gmail.com>
2023-06-22 03:49:45 -0700

    some stuff to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DOOM
+
+just a little practice.
+
+# Enter at your own risk!!!
+
+_oooo scary_
---
+++ script.js
@@ -0,0 +1,74 @@
+const countdown = (input) => {
+    const countContainer = document.getElementById('countContainer')
+
+    for (let i = input; i >= 0; i--){
+         
+        setTimeout(function () {
+            renderCount(i)
+        }, (input - i) * 1000)
+        if (i === 0) {
+            setTimeout(function () {
+                renderCount('KABOOM!!!')
+            }, input * 1000)
+        }
+        
+        }
+    
+    
+
+    
+}
+
+const renderCount = (value) => {
+    const countNum = document.getElementById('countNum')
+    countNum.textContent = value 
+    
+
+}
+    
+
+
+
+const startCount = (event) => {
+    event.preventDefault()
+    const num = document.getElementById('num')
+    const numInput = parseInt(num.value)
+    countdown(numInput)
+    
+}
+
+document.getElementById('doom').addEventListener('click', function (event) {
+    startCount(event)
+})
+
+// const countdown = (input) => {
+//   const countContainer = document.getElementById('countContainer');
+
+//   for (let i = input; i >= 0; i--) {
+//     setTimeout(function() {
+//       renderCount(i);
+//     }, (input - i) * 1000);
+
+//     if (i === 0) {
+//       setTimeout(function() {
+//         renderCount('KABOOM!!!');
+//       }, input * 1000);
+//     }
+//   }
+// };
+
+// const renderCount = (value) => {
+//   const countNum = document.getElementById('countNum');
+//   countNum.textContent = value;
+// };
+
+// const startCount = (event) => {
+//   event.preventDefault();
+//   const num = document.getElementById('num');
+//   const numInput = parseInt(num.value);
+//   countdown(numInput);
+// };
+
+// document.getElementById('doom').addEventListener('click', startCount);
+
+
